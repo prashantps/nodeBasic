d0558742dbaf702c9b4575a6173a636aee1fa18c
prashantps <android.prashant.ps@gmail.com>
2016-07-19 17:45:54 +0530

    updated http listen for hearoku

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(5000);
-
-// Console will print the message
-console.log('Server running at http://localhost:5000/');
+}).listen(process.env.PORT || 3000, function(){
+	console.log('listening on', http.address().port);
+});
